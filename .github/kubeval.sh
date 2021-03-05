575fad65ede636c6e2b325d96481633e4cb4c079
Luca Bertelli <luca.bertelli@kiratech.it>
2021-03-05 12:12:27 +0100

    Fix Schema location for kubeval

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 KUBEVAL_VERSION="0.14.0"
-SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/garethr"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
